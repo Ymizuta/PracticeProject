28e653c7918b9becb275921b80f0e45edbce9f06
Ymizuta <suiden.53@gmail.com>
2019-02-11 13:25:06 +0900

    [add][エディター拡張]任意のオブジェクトを座標を指定して生成する機能を追加

--- Assets/Editor/ObjCreateEditor.cs
+++ Assets/Editor/ObjCreateEditor.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-public class SampleMapCreater : EditorWindow {
-
+public class ObjCreateEditor : EditorWindow {
     private float x_poz_;
     private float y_poz_;
     private float z_poz_;
+    private Vector3 instantiate_poz_;
     private Object instantiated_object_;
 
-    [UnityEditor.MenuItem("Editor/SampleMapCreater")]
+    [UnityEditor.MenuItem("Editor/ObjCreateEditor")]
     private static void Create()
     {
-        EditorWindow.GetWindow<SampleMapCreater>("SampleMapCreater");
+        EditorWindow.GetWindow<ObjCreateEditor>("ObjectCreateEditor");
     }
 
     private void OnGUI()
@@ -21,7 +21,7 @@ public class SampleMapCreater : EditorWindow {
         float default_x = 0.0f;
         float x = 0.0f;
         float y = 0.0f;
-        float w = 50.0f;        
+        float w = 50.0f;
         float maxw = 300.0f;
         float h = 50.0f;
 
@@ -29,16 +29,16 @@ public class SampleMapCreater : EditorWindow {
         using (new EditorGUILayout.HorizontalScope())
         {
             GUILayout.Label("生成オブジェクト:", GUILayout.Width(100));
-            instantiated_object_ = EditorGUILayout.ObjectField(instantiated_object_,typeof(GameObject)) as GameObject;
+            instantiated_object_ = EditorGUILayout.ObjectField(instantiated_object_, typeof(GameObject)) as GameObject;
         }
         using (new EditorGUILayout.HorizontalScope())
         {
-            GUILayout.Label("X:",GUILayout.Width(100));
+            GUILayout.Label("X:", GUILayout.Width(100));
             x_poz_ = EditorGUILayout.FloatField(0);
         }
         using (new EditorGUILayout.HorizontalScope())
         {
-            GUILayout.Label("y:",GUILayout.Width(100));
+            GUILayout.Label("y:", GUILayout.Width(100));
             y_poz_ = EditorGUILayout.FloatField(0);
         }
         using (new EditorGUILayout.HorizontalScope())
@@ -47,8 +47,14 @@ public class SampleMapCreater : EditorWindow {
             z_poz_ = EditorGUILayout.FloatField(0);
         }
 
-        GUILayout.Button("オブジェクト生成");
-              
+        if (GUILayout.Button("オブジェクト生成"))
+        {
+            instantiate_poz_.x = x_poz_;
+            instantiate_poz_.y = y_poz_;
+            instantiate_poz_.z = z_poz_; 
+            GameObject new_object = Instantiate(instantiated_object_,instantiate_poz_,Quaternion.identity) as GameObject;
+        }
+            
         //GUILayout.BeginHorizontal();
         //for (int i = 1; i<10; i++)
         //{
@@ -66,5 +72,6 @@ public class SampleMapCreater : EditorWindow {
         //    Rect r = new Rect(new Vector2(x,y),new Vector2(w,h));
         //}
     }
+
 
 }
