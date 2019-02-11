0b165d5c26464b35df9120ae0cc62198a98edc0f
Ymizuta <suiden.53@gmail.com>
2019-02-11 15:27:05 +0900

    [add]マップエディターの準備

--- Assets/Editor/ObjCreateEditor.cs
+++ Assets/Editor/ObjCreateEditor.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-public class ObjCreateEditor : EditorWindow {
+public class ObjCreateEditor : EditorWindow{
     private float x_poz_;
     private float y_poz_;
     private float z_poz_;
     private Vector3 instantiate_poz_;
     private Object instantiated_object_;
+
+    //private int number_of_instances_;
+    //private float min_x_;
+    //private float max_x_;
+    //private float min_y_;
+    //private float max_y_;
+    //private float min_z_;
+    //private float max_z_;
 
     [UnityEditor.MenuItem("Editor/ObjCreateEditor")]
     private static void Create()
@@ -18,14 +26,7 @@ public class ObjCreateEditor : EditorWindow {
 
     private void OnGUI()
     {
-        float default_x = 0.0f;
-        float x = 0.0f;
-        float y = 0.0f;
-        float w = 50.0f;
-        float maxw = 300.0f;
-        float h = 50.0f;
-
-        //後で関数化する
+        GUILayout.Label("個別オブジェクト生成");
         using (new EditorGUILayout.HorizontalScope())
         {
             GUILayout.Label("生成オブジェクト:", GUILayout.Width(100));
@@ -34,17 +35,17 @@ public class ObjCreateEditor : EditorWindow {
         using (new EditorGUILayout.HorizontalScope())
         {
             GUILayout.Label("X:", GUILayout.Width(100));
-            x_poz_ = EditorGUILayout.FloatField(0);
+            x_poz_ = EditorGUILayout.FloatField(x_poz_);
         }
         using (new EditorGUILayout.HorizontalScope())
         {
             GUILayout.Label("y:", GUILayout.Width(100));
-            y_poz_ = EditorGUILayout.FloatField(0);
+            y_poz_ = EditorGUILayout.FloatField(y_poz_);
         }
         using (new EditorGUILayout.HorizontalScope())
         {
-            GUILayout.Label("y:", GUILayout.Width(100));
-            z_poz_ = EditorGUILayout.FloatField(0);
+            GUILayout.Label("z:", GUILayout.Width(100));
+            z_poz_ = EditorGUILayout.FloatField(z_poz_);
         }
 
         if (GUILayout.Button("オブジェクト生成"))
@@ -54,24 +55,96 @@ public class ObjCreateEditor : EditorWindow {
             instantiate_poz_.z = z_poz_; 
             GameObject new_object = Instantiate(instantiated_object_,instantiate_poz_,Quaternion.identity) as GameObject;
         }
-            
+        EditorGUILayout.Space();
+
+        
+        ///*
+        // *マップ 
+        // */
+        //float default_x = 0.0f;
+        //float x = 0.0f;
+        //float y = 0.0f;
+        //float w = 50.0f;
+        //float maxw = 300.0f;
+        //float h = 50.0f;
+
         //GUILayout.BeginHorizontal();
-        //for (int i = 1; i<10; i++)
+        //for (int i = 0; i <= 10; i++)
         //{
         //    x += w;
         //    if (x > maxw)
         //    {
         //        x = default_x;
+        //        y += h;
         //        GUILayout.EndHorizontal();
         //    }
         //    if (x == default_x)
         //    {
         //        GUILayout.BeginHorizontal();
         //    }
-        //    GUILayout.Button("ボタン" + i, GUILayout.Width(w), GUILayout.Height(h));
         //    Rect r = new Rect(new Vector2(x,y),new Vector2(w,h));
+        //    DrawGridLine(r);
         //}
-    }
-
-
+        //GUILayout.EndHorizontal();    
 }
+
+private void DrawGridLine(Rect r)
+    {
+        // grid
+        Handles.color = new Color(1f, 1f, 1f, 0.5f);
+        // upper line
+        Handles.DrawLine(
+            new Vector2(r.position.x, r.position.y),
+            new Vector2(r.position.x + r.size.x, r.position.y));
+        // bottom line
+        Handles.DrawLine(
+            new Vector2(r.position.x, r.position.y + r.size.y),
+            new Vector2(r.position.x + r.size.x, r.position.y + r.size.y));
+        // left line
+        Handles.DrawLine(
+            new Vector2(r.position.x, r.position.y),
+            new Vector2(r.position.x, r.position.y + r.size.y));
+        // right line
+        Handles.DrawLine(
+            new Vector2(r.position.x + r.size.x, r.position.y),
+            new Vector2(r.position.x + r.size.x, r.position.y + r.size.y));
+    }
+}
+
+///*
+// *ランダムオブジェクト生成 
+// */
+//GUILayout.Label("ランダムオブジェクト生成");
+//using (new EditorGUILayout.HorizontalScope())
+//{
+//    GUILayout.Label("生成オブジェクト:", GUILayout.Width(100));
+//    instantiated_object_ = EditorGUILayout.ObjectField(instantiated_object_, typeof(GameObject)) as GameObject;
+//}
+//using (new EditorGUILayout.HorizontalScope())
+//{
+//    GUILayout.Label("生成個数:", GUILayout.Width(100));
+//    number_of_instances_ = EditorGUILayout.IntField(number_of_instances_);
+//}
+//using (new EditorGUILayout.HorizontalScope())
+//{
+//    GUILayout.Label("生成範囲X:", GUILayout.Width(100));
+//    min_x_ = EditorGUILayout.FloatField(min_x_);
+//    max_x_ = EditorGUILayout.FloatField(max_x_);
+//}
+//using (new EditorGUILayout.HorizontalScope())
+//{
+//    GUILayout.Label("生成範囲y:", GUILayout.Width(100));
+//    min_y_ = EditorGUILayout.FloatField(min_y_);
+//    max_y_ = EditorGUILayout.FloatField(max_y_);
+//}
+//using (new EditorGUILayout.HorizontalScope())
+//{
+//    GUILayout.Label("生成範囲z:", GUILayout.Width(100));
+//    min_z_ = EditorGUILayout.FloatField(min_z_);
+//    max_z_ = EditorGUILayout.FloatField(max_z_);
+//}
+//if (GUILayout.Button("ランダムオブジェクト生成"))
+//{
+
+//}
+
